1420284bb68d9757d811d535f7461a5e18fa3d36
Fernanda <febeltraoleite@gmail.com>
2025-03-22 00:15:34 -0300

    Feat : Adiciona página de códigos

---
+++ verificadorDePalindromos.py
@@ -0,0 +1,12 @@
+caracter = "radar"
+
+def verifica(caracter):
+    
+    caracter= caracter.lower()
+    
+    if caracter == caracter [::-1]:
+        return True
+    else: 
+        return False
+    
+print(verifica(caracter))
